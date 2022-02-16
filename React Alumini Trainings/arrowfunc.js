281b2add01af587c50061ec477a61c8ac1d85a51
dhaneshvar <dhaneshvar2912@gmail.com>
2022-02-16 23:25:00 +0530

    2022-02-12 Saturday Introduction Tranings

---
+++ React Alumini Trainings/arrowfunc.js
@@ -0,0 +1,10 @@
+const funcname = name => 	"hi ${name}"
+console.log(funcname("Bavi"))
+
+const arr = ['sdd','ddf']
+let cal = arr.map(function(item))
+{
+	return item;
+}
+let call = arr.map(item => item)
+console.log(call)
