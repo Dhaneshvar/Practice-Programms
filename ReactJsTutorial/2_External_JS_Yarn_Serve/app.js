15a0b845a73f4520f2aa8dd621b7b1bf6d2d1808
dhaneshvar <dhaneshvar2912@gmail.com>
2022-02-24 23:12:48 +0530

    ReactJs Tutorial External JS 2022-02-24

---
+++ ReactJsTutorial/2_External_JS_Yarn_Serve/app.js
@@ -0,0 +1,5 @@
+function Hello()
+{
+	 return <h1>Installed npm install yarn, then yarn add serve, then yarn serve . or yarn run serve</h1>;
+}
+ReactDOM.render(<Hello />, document.getElementById('mydiv'))
